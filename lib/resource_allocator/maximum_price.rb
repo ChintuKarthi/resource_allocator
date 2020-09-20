c30a06fb1c8598c81b8df4949f23d5b5bb1f2d7d
Karthikeyan Dhanapal <chintukarthi@gmail.com>
2020-09-20 21:03:48 +0530

    logic updated to honour float price

--- lib/resource_allocator/maximum_price.rb
+++ lib/resource_allocator/maximum_price.rb
@@ -12,13 +12,15 @@ module MaximumPrice
     # Stringifying and Inverting the keys so that price can be passed to optimize servers method.
     # Price and region cost is multplied by 1000 to make the cost as a whole number.
     call_flag = 1
-    converted_east_cost = Hash[$region[:'us-east'].map { |k, v| [k, (v * 1000)] }]
-    converted_west_cost = Hash[$region[:'us-west'].map { |k, v| [k, (v * 1000)] }]
-    converted_asia_cost = Hash[$region[:'asia'].map { |k, v| [k, (v * 1000)] }]
+    converted_east_cost = Hash[$region[:'us-east'].map { |k, v| [k, (v * 10000).to_i] }]
+    converted_west_cost = Hash[$region[:'us-west'].map { |k, v| [k, (v * 10000).to_i] }]
+    converted_asia_cost = Hash[$region[:'asia'].map { |k, v| [k, (v * 10000).to_i] }]
+
+    price = ((price/hours.to_f)*10000)
     
-    east_server = OptimizeCPU.optimize_servers(converted_east_cost.values, price*1000, $region[:'us-east'].stringify_keys.invert, call_flag)
-    west_server = OptimizeCPU.optimize_servers(converted_west_cost.values, price*1000, $region[:'us-west'].stringify_keys.invert, call_flag)
-    asia_server = OptimizeCPU.optimize_servers(converted_asia_cost.values, price*1000, $region[:'asia'].stringify_keys.invert, call_flag)
+    east_server = OptimizeCPU.optimize_servers(converted_east_cost.values, price.to_i, $region[:'us-east'].stringify_keys.invert, call_flag)
+    west_server = OptimizeCPU.optimize_servers(converted_west_cost.values, price.to_i, $region[:'us-west'].stringify_keys.invert, call_flag)
+    asia_server = OptimizeCPU.optimize_servers(converted_asia_cost.values, price.to_i, $region[:'asia'].stringify_keys.invert, call_flag)
 
     east_hash = {}
     west_hash = {}
@@ -26,21 +28,21 @@ module MaximumPrice
     if east_server.present?
       east_hash = {
         "region": "us-east",
-        "total_cost": "$" + price.to_s,
+        "total_cost": "$" + (((price.to_f)*8)/10000).to_s,
         "servers": east_server
       }
     end
     if west_server.present?
       west_hash = {
         "region": "us-west",
-        "total_cost": "$" + price.to_s,
+        "total_cost": "$" + (((price.to_f)*8)/10000).to_s,
         "servers": west_server
       }
     end
     if asia_server.present?
       asia_hash = {
         "region": "asia",
-        "total_cost": "$" + price.to_s,
+        "total_cost": "$" + (((price.to_f)*8)/10000).to_s,
         "servers": asia_server
       }
     end
